2c24826d8028d2a77360e02cccb5c55d35e19fd2
victordriva <82041390+victordriva@users.noreply.github.com>
2022-02-15 07:40:22 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -7,6 +7,8 @@ sudo rm -r $ZSH
 
 sudo apt install -y curl wget zsh git python3 python3-pip
 
+git config --global init.defaultBranch main
+
 echo "Obtendo oh-my-zsh"
 export RUNZSH=no
 export ZSHPLUGINS=$HOME/plugins
@@ -31,9 +33,9 @@ export DRIVAHOME=$HOME/driva
 echo "export DRIVAHOME=$HOME/driva" >> $HOME/.zshrc
 mkdir $DRIVAHOME
 
-wget https://github.com/cli/cli/releases/download/v2.0.0/gh_2.0.0_linux_amd64.deb
-sudo apt install ./gh_2.0.0_linux_amd64.deb
-rm gh_2.0.0_linux_amd64.deb
+wget https://github.com/cli/cli/releases/download/v2.5.0/gh_2.5.0_linux_amd64.deb
+sudo apt install ./gh_2.5.0_linux_amd64.deb
+rm gh_2.5.0_linux_amd64.deb
 
 gh auth login
 
@@ -51,5 +53,23 @@ nvm install 14.17.6
 npm install --global yarn
 
 zsh $HOME/.zshrc
+
+
+echo "Configurando desenvolvimento Python"
+
+sudo apt-get install -y make build-essential libssl-dev zlib1g-dev \
+libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm \
+libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+rm -rf $HOME/.pyenv
+
+curl https://pyenv.run | bash
+
+echo 'export PATH="$HOME/.pyenv/bin:$PATH"' >> $HOME/.zshrc
+echo 'eval "$(pyenv init --path)"' >> $HOME/.zshrc
+echo 'eval "$(pyenv virtualenv-init -)"' >> $HOME/.zshrc
+
+
+
 
 echo "FINALIZADO"
